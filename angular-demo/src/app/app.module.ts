87bcb37a0d629578926697643625f6bf1ef757f6
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2022-03-18 09:13:10 +0300

    feat: add paths in tsconfig file, add some logic in material components

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSliderModule } from "@angular/material/slider";
-import { TestComponent } from "./components/test/test.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { TestComponent } from "@app/components";
 
 @NgModule({
   declarations: [AppComponent, TestComponent],
@@ -14,6 +16,9 @@ import { TestComponent } from "./components/test/test.component";
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- angular-demo/src/app/components/test/test.component.ts
+++ angular-demo/src/app/components/test/test.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./test.component.scss"],
 })
 export class TestComponent implements OnInit {
-  constructor() {}
+  title = "Angular Material Demo";
+  count = 0;
 
   ngOnInit(): void {
     console.log("INIT");
+  }
+
+  updateTitle(evt: Event): string {
+    const { value } = evt.target as HTMLInputElement;
+    if (!value) {
+      return (this.title = "Angular Material Demo");
+    }
+    return (this.title = value);
+  }
+
+  increase(): void {
+    this.count++;
   }
 }
